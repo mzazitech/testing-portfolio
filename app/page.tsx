1560504bb34c07b592c89739f48d51bef589a9b9
mzazitech <mzazitech001@gmail.com>
2026-02-21 13:00:54 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero"
+import TechStack from "@/components/TechStack"
+import Terminal from "@/components/Terminal"
+
+export default function Home() {
+  return (
+    <main className="px-6 md:px-20">
+      <Hero />
+      <TechStack />
+      <Terminal />
+    </main>
+  )
+}

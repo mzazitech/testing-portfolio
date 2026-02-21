53f8daea7e646cc710cb3e5b5af6f4f4d008340c
mzazitech <mzazitech001@gmail.com>
2026-02-21 13:05:12 +0300

    Create projects.ts

---
+++ data/projects.ts
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "Mzazi Bot v1.0",
+    description: "Advanced WhatsApp bot built with Baileys",
+  },
+  {
+    title: "VPN Platform",
+    description: "Tech website selling VPN configs",
+  },
+]
